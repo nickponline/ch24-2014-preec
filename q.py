b70b20b7b879b0e806c04a9e074ddf79aa4c51ea
Nicholas Pilkington <nicholas.pilkington@gmail.com>
2014-02-01 13:11:58 +0200

    Updating q

--- q.py
+++ q.py
@@ -8,76 +8,36 @@ import pickle
 import sys
 import pylab
 from scipy import stats
+import scipy.stats
+import numpy
+import scipy
+import pylab
 
-def goetrzel(x, target_frequency, sample_rate):
-	s_prev = 0
-	s_prev2 = 0
-	normalized_frequency = target_frequency / sample_rate
-	coeff = 2.0 * np.cos(2.0 * np.pi * normalized_frequency)
-	for sample in x:
+def goertzel(samples, target_frequency, sample_rate):
+	s_prev = 0 
+	s_prev2 = 0 
+	normalized_frequency = 1.0 * target_frequency / sample_rate 
+	coeff = 2.0 * np.cos(2.0 * np.pi * normalized_frequency) 
+	for sample in samples:
 	  s = sample + coeff * s_prev - s_prev2
 	  s_prev2 = s_prev
 	  s_prev = s
-	power = s_prev2 * s_prev2 + s_prev * s_prev - coeff * s_prev * s_prev2 ;
+	power = s_prev2 * s_prev2 + s_prev * s_prev - coeff * s_prev * s_prev2
 	return power
 
 sample_rate, data = scipy.io.wavfile.read(sys.argv[1])
-print sample_rate
+window_length = 10100
 
-total = None
-
-for tf in xrange(500, 10100, 100):
-
-	print tf
-	length = int(2*tf) # 
-	skip   = 500 # 0.1s precision
+for i in xrange(1, len(data) / sample_rate):
 	
-	windows = [data[start:start+length] for start in xrange(0, len(data) - length, skip)] 
-	fourier = np.array([goetrzel(w, tf, sample_rate) for w in windows], dtype=np.float64)
-	n       = len(fourier)
-	time    = np.arange(n, dtype=np.float64) / n * len(data) / sample_rate
+	window = data[i*sample_rate - window_length / 2:i*sample_rate + window_length/2]
+	freq = np.array([goertzel(window, fs * 100, sample_rate) for fs in xrange(5, 101)])
+	c = freq.mean()
 
-	c = np.copy(fourier)
-	M = stats.scoreatpercentile(c, 95)
-	print c.min(), c.max(), c.mean(), M
-	
-	pylab.figure()
-	pylab.subplot(211)
-	pylab.plot(time, fourier)
-	pylab.plot(time, [M] * len(time))
+	# pylab.figure()
+	# pylab.title(str(i))
+	# pylab.plot([fs*100 for fs in xrange(5, 101)], freq, 'ro')
+	# pylab.plot([fs*100 for fs in xrange(5, 101)], [c for fs in xrange(5, 101)] )
+	# pylab.show()
 
-	idx = np.where( c > M )
-	fourier[idx] = 1
-	idx = np.where( c <= M )
-	fourier[idx] = 0
-
-	
-	if total is None:
-		total = fourier
-	else:
-		fourier = np.pad(fourier, len(total) - len(fourier), mode='minimum')
-		total += fourier
-
-	pylab.subplot(212)
-	pylab.plot(time, total,'r') 
-	pylab.show()
-
-	peakValue = total.max()
-	startPeakIndex = 0
-
-	while startPeakIndex < len(time):
-
-		if total[startPeakIndex] == peakValue:
-			endPeakIndex = startPeakIndex + 1
-			while endPeakIndex < len(time) and (total[endPeakIndex] == total[startPeakIndex]):
-				endPeakIndex += 1
-			endPeakIndex -= 1
-			duration = time[endPeakIndex] - time[startPeakIndex]
-			print "Peak duration: ", duration, "Start:", time[startPeakIndex], "Value: ", peakValue
-			
-			if duration > 1.0:
-				print int(peakValue), (time[startPeakIndex] + duration / 2.0)
-			startPeakIndex = endPeakIndex + 1
-
-		else:
-			startPeakIndex += 1
+	print " Second: ", i, len(freq[freq > c])
--- r.py
+++ r.py
@@ -2,5 +2,5 @@ import cv2
 import sys
 
 image = cv2.imread(sys.argv[1])
-cv2.imshow('Cineam', image)
+cv2.imshow('Cinema', image)
 cv2.waitKey(0)
